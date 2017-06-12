2f725e6ec4abd58ba14dce7ee013180fd722d079
propol <propol_96@hotmail.com>
2017-06-12 13:39:46 +0300

    Add files via upload
    Assets used in my demo

---
+++ CharController_Demo/Assets/_Scripts/TP_Animator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Animator : MonoBehaviour {
+
+	public enum Direction
+	{
+		Stationary, Forward, Backward, Left, Right,
+		LeftForward, RightForward, LeftBackward, RightBackward
+	}
+
+	public Direction MoveDirection { get; set; }
+
+	public static TP_Animator Instance;
+
+	void Awake () {
+		Instance = this;
+	}
+
+	void Update () {
+		
+	}
+
+	public void DetermineCurrentMoveDirection(){
+
+		var forward = false;
+		var backward = false;
+		var left = false;
+		var right = false;
+
+		if (TP_Motor.Instance.MoveVector.z > 0)
+			forward = true;
+
+		if (TP_Motor.Instance.MoveVector.z < 0)
+			backward = true;
+
+		if (TP_Motor.Instance.MoveVector.x > 0)
+			right = true;
+
+		if (TP_Motor.Instance.MoveVector.x < 0)
+			left = true;
+
+		if (forward) {
+
+			if (left)
+				MoveDirection = Direction.LeftForward;
+			else if (right)
+				MoveDirection = Direction.RightForward;
+			else
+				MoveDirection = Direction.Forward;
+
+		} else if (backward) {
+
+			if (left)
+				MoveDirection = Direction.LeftBackward;
+			else if (right)
+				MoveDirection = Direction.RightBackward;
+			else
+				MoveDirection = Direction.Backward;
+
+		} else if (left) {
+
+			MoveDirection = Direction.Left;
+
+		} else if (right) {
+
+			MoveDirection = Direction.Right;
+
+		} else {
+			
+			MoveDirection = Direction.Stationary;
+
+		}
+	}
+}
---
+++ CharController_Demo/Assets/_Scripts/TP_Controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_Controller : MonoBehaviour {
+
+	public static CharacterController CharacterController;
+	public static TP_Controller Instance;
+
+	void Awake () {
+		CharacterController = GetComponent ("CharacterController") as CharacterController;
+		Instance = this;
+		TP_Camera.UseExistingOrCreateNewMainCamera ();
+	}
+
+	void Update () {
+		if (Camera.main == null)
+			return;
+		
+		GetLocomotionInput ();
+		HandleActionInput ();
+
+		TP_Motor.Instance.UpdateMotor ();
+	}
+
+	void GetLocomotionInput(){
+	
+		var deadZone = 0.1f;
+
+		TP_Motor.Instance.VerticalVelocity = TP_Motor.Instance.MoveVector.y;
+		TP_Motor.Instance.MoveVector = Vector3.zero;
+
+		if(Input.GetAxis("Vertical") > deadZone || Input.GetAxis("Vertical") < -deadZone){
+			TP_Motor.Instance.MoveVector += new Vector3 (0,0,Input.GetAxis("Vertical")); 
+		}
+
+		if(Input.GetAxis("Horizontal") > deadZone || Input.GetAxis("Horizontal") < -deadZone){
+			TP_Motor.Instance.MoveVector += new Vector3 (Input.GetAxis("Horizontal"), 0 ,0); 
+		}
+
+		TP_Animator.Instance.DetermineCurrentMoveDirection ();
+	}
+
+	void HandleActionInput(){
+
+		if (Input.GetButton ("Jump")) {
+			Jump ();
+		}
+
+	}
+
+	void Jump(){
+
+		TP_Motor.Instance.Jump ();
+	}
+}
